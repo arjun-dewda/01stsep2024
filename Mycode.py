dd5def163b1fea59bb30d73bc429865f1160e642
arjun-dewda <arjundewda88@gmail.com>
2024-09-01 11:36:46 +0530

    code changed

--- Mycode.py
+++ Mycode.py
@@ -3,8 +3,11 @@
 # Note: change this value for a different result
 num = 8 
 
+
 # To take the input from the user
 #num = float(input('Enter a number: '))
 
 num_sqrt = num ** 0.5
 print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+
+#chnages done. Lets upload it to github.
